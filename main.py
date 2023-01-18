1497fbf70f82057d8d4a4462ec9b60adc90e510b
Roldan Dave Talledo <talledoroldan@gmail.com>
2023-01-18 09:19:28 +0800

    files update

--- main.py
+++ main.py
@@ -134,52 +134,6 @@ def BFS(inputState):
     return 0
 
 
-def DFS(inputState):
-    # generating start states of variables and data structures used in the algorithm
-    start_time = time.time()
-    stack = []
-    explored = {}
-    parent = {}
-    parent_cost = {}
-    integer_state = int(inputState)
-    parent_cost[integer_state] = 0
-    explored[integer_state] = 1
-    stack.append(integer_state)
-    cnt = 0
-    global dfs_counter
-    global dfs_path
-    global dfs_cost
-    global dfs_depth
-    global time_dfs
-    dfs_depth = 0
-    while stack:
-        cnt += 1
-        state = stack[-1]
-        stack.pop()
-        dfs_depth = max(dfs_depth, parent_cost[state])
-        if goalTest(state):
-            path = getPath(parent, int(inputState))
-            # printPath(path)
-            dfs_counter = cnt
-            dfs_path = path
-            dfs_cost = len(path) - 1
-            time_dfs = float(time.time() - start_time)
-            return 1
-        # generating childeren
-        children = getChildren(getStringRepresentation(state))
-        for child in children:
-            child_int = int(child)
-            if child_int not in explored:
-                stack.append(child_int)
-                parent[child_int] = state
-                explored[child_int] = 1
-                parent_cost[child_int] = 1 + parent_cost[state]
-    dfs_path = []
-    dfs_cost = 0
-    dfs_counter = cnt
-    time_dfs = float(time.time() - start_time)
-    return 0
-
 
 # function checking if state is valid or out of bounds
 def checkValid(i, j):
@@ -202,17 +156,6 @@ def getManhattanDistance(state):
 
 
 # heuristic function using manhattan distance
-
-def getEuclideanDistance(state):
-    tot = 0
-    for i in range(1, 9):
-        goalX = int(i / 3)
-        goalY = i % 3
-        idx = state.index(str(i))
-        itemX = int(idx / 3)
-        itemY = idx % 3
-        tot += math.sqrt(pow((goalX - itemX), 2) + pow((goalY - itemY), 2))
-    return tot
 
 
 def AStarSearch_manhattan(inputState):
@@ -276,63 +219,6 @@ def AStarSearch_manhattan(inputState):
     return 0
 
 
-def AStarSearch_euclid(inputState):
-    # generating start states of variables and data structures used in the algorithm
-    start_time = time.time()
-    integer_state = int(inputState)
-    heap = []
-    explored = {}
-    parent = {}
-    cost_map = {}
-    heapq.heappush(heap, (getEuclideanDistance(inputState), integer_state))
-    cost_map[integer_state] = getEuclideanDistance(inputState)
-    heap_map = {}
-    heap_map[integer_state] = 1
-    global euclid_counter
-    global euclid_path
-    global euclid_cost
-    global euclid_depth
-    global time_euclid
-    euclid_depth = 0
-    while heap:
-        node = heapq.heappop(heap)
-        state = node[1]
-        string_state = getStringRepresentation(state)
-        parent_cost = node[0] - getEuclideanDistance(string_state)
-        # handling the nodes that was renewed
-        if not state in explored:
-            euclid_depth = max(parent_cost, euclid_depth)
-        explored[state] = 1
 
-        if goalTest(state):
-            path = getPath(parent, int(inputState))
-            # printPath(path)
-            euclid_path = path
-            euclid_counter = (len(explored))
-            euclid_cost = len(path) - 1
-            time_euclid = float(time.time() - start_time)
-
-            return 1
-        # generating childeren
-        children = getChildren(string_state)
-        for child in children:
-            new_cost = getEuclideanDistance(child)
-            child_int = int(child)
-            if child_int not in explored and not child in heap_map:
-                heapq.heappush(heap, (parent_cost + new_cost + 1, child_int))
-                heap_map[child_int] = 1
-                cost_map[child_int] = parent_cost + new_cost + 1
-                parent[child_int] = state
-            elif child_int in heap_map:
-                if (new_cost + parent_cost + 1) < cost_map[child_int]:
-                    parent[child_int] = state
-                    cost_map[child_int] = new_cost + parent_cost + 1
-                    heapq.heappush(heap, (parent_cost + 1 + new_cost, child_int))
-    euclid_cost = 0
-    euclid_path = []
-    euclid_counter = (len(explored))
-    time_euclid = float(time.time() - start_time)
-
-    return 0
 
 
